f4272cf5997bebe82d9f1b26944a62d9fcbf04a4
CaioSantosOggi <caio.santos@oggisorvetes.com.br>
2026-03-07 11:28:16 -0300

    feat: app updates

--- App.js
+++ App.js
@@ -1,7 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as LocalAuthentication  from 'expo-local-authentication';
+import { useEffect,useState } from 'react';
+
+
+export function telasegura(){
+  const [access, setAccess] = useState(false);
+  
+  useEffect(() => {
+    (async () => {
+      const authentication = await LocalAuthentication.authenticateAsync();
+      if (authentication.success)
+        setAccess(true)
+      
+      else
+        setAccess(false)
+    })();
+  }, []);
+
+}
+
+return(
+  <View>
+    {access &&(
+      <Text>Usuário logado com sucesso!</Text>
+    )}
+  </View>
+)
 
 export default function App() {
+
+  const [biometria,setBiometria] = useState(false);
+  const [render, setRender] = useState(false);
+
+  const changRender = () =>setRender(true)  
+
+  useEffect(() => {
+    (async () => {
+      const compativel = await LocalAuthentication.hasHardwareAsync();
+      setBiometria(compativel)
+    })();
+  }, []);
+
+  if(render){
+    return <telasegura/>
+  } else{
+    return(
+      <View style={styles.container}>
+        <Text>
+          {biometria ? 'Biometria disponivel' : 'Biometria não disponivel'}
+        </Text>
+        <TouchableOpacity onPress={changRender}>
+          <Text>Entrar</Text>
+        </TouchableOpacity>
+        <statusbar style='auto'/>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
